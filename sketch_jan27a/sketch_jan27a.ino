45f989593c68460c0ca3d38208a1e5f6f1216ba1
Nate Potato <nathanielthomasholt@gmail.com>
2022-01-29 18:21:23 -0500

    weekly update

--- sketch_jan27a/sketch_jan27a.ino
+++ sketch_jan27a/sketch_jan27a.ino
@@ -6,14 +6,14 @@
 // the setup function runs once when you press reset or power the board
  
 void setup() {  // initialize digital pin 13 as an output.
-   pinMode(10, OUTPUT);
+   pinMode(13, OUTPUT);
 }
  
 // the loop function runs over and over again forever
  
 void loop() {
-   digitalWrite(5000, HIGH); // turn the LED on (HIGH is the voltage level)
-   delay(5000); // wait for a second
+   digitalWrite(500, HIGH); // turn the LED on (HIGH is the voltage level)
+   delay(500); // wait for a second
    digitalWrite(200, LOW); // turn the LED off by making the voltage LOW
    delay(1000); // wait for a second
 }
